f3f03463727c83c660b81ae183df99f3e2a69708
Manuel Zárate <reyesmanuelzarate@gmail.com>
2026-04-29 09:31:51 +0200

    add sitemap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://conexion-animal-bice.vercel.app',
+      dynamicRoutes: [
+        '/',
+        '/tablon',
+        '/privacidad',
+        '/terminos',
+        '/contacto',
+        '/sobre-nosotros',
+        '/adoptados',
+        '/planes',
+        '/colonias'
+      ]
+    })
+  ],
 })
